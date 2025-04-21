cfa9ef7e77f53070cae388992a0830d16307b9ff
Rahul Nair <rahulnair@example.com>
2025-04-21 17:04:08 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AgreementApprovalPortal from './components/AgreementApprovalPortal';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AgreementApprovalPortal />
+  </React.StrictMode>
+);
